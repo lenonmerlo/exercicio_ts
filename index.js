3ec445c71afc3fefcc0e01b55d606b68933fe260
lenonmerlo <lenontm@gmail.com>
2024-09-19 09:35:21 -0300

    Exercicio Typescript feito para o curso Front-end EBAC

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Testando as funções
+console.log(multiplicar(5, 3)); // 15
+console.log(saudacao("Lenon")); // Olá Lenon
---
+++ index.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Testando as funções
+console.log(multiplicar(5, 3)); // 15
+console.log(saudacao("Lenon")); // Olá Lenon
